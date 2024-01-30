d9899374b83d83c32ef0a7fb229aa50f417497ef
Reydor <uzbbnbf@gmail.com>
2024-01-30 20:00:14 +0500

    ustoz qiynalmasligiz uchun

--- uyga vazifa/v4.py
+++ uyga vazifa/v4.py
@@ -1,40 +1,31 @@
 class Circle:
-    def __init__(self, radius:int, color:str):
+    def __init__ (self, radius:int, color:str):
         self.radius=radius
         self.color=color
     def get_radius(self):
-        a=self.radius
-        return a
-    def set_radius(self, new_radius:int):
-        self.radius=new_radius
-        print(self.radius)
+        return self.radius
+    def set_radius(self, newRadius:int):
+        self.radius=newRadius
     def get_color(self):
         return self.color
-    def set_color(self, new_color:str):
-        self.color=new_color
-        return self.color
-    def get_area(self):
+    def set_color(self, newColor:str):
+        self.color=newColor
+    def yuza(self):
         return self.radius**2*3.14
-    def circuremence (self):
-        return 2*3.14*self.radius
-    def get_info(self):
+    def aylana_uzunligi(self):
+        return 2*self.radius*3.14
+    def info(self):
         return f"""
-    radius:             {self.get_radius()}
-    color :             {self.get_color()}
-    change radius :     {self.set_radius(100)}
-    change color :      {self.set_color("blue")}
-    S:                  {self.get_area()}
-    C:                  {self.circuremence()}
+    aylana radiusi  : {self.radius}
+    aylana rangi    : {self.color}
+    aylana yuzasi   : {self.yuza()}
+    aylana uzunligi : {self.aylana_uzunligi()}    
     """
-    
-
-
-o1=Circle(10, "red")
-print(o1.get_radius())
-print(o1.set_radius(100))
-print(o1.get_color())
-print(o1.get_area())
-print(o1.set_color("blue"))
-print(o1.get_area())
-print(o1.circuremence())
-print(o1.get_info())
+a=Circle(10, "blue")
+print(a.get_radius())
+print(a.get_color())
+a.set_radius(20)
+a.set_color("white")
+print(a.yuza())
+print(a.aylana_uzunligi())
+print(a.info())
